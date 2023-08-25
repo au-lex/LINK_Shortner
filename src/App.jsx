6ce36d2c59c51e10b37c66509c017fbe728f4e89
au-lex <aulexc4d2@gmail.com>
2023-08-25 10:28:51 +0100

    done with with the url and functionality

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import './App.css'
+import Head from './Compo/Head'
+import Hero from './Compo/Hero'
+import Footer from './Compo/Footer'
+
+
+
+const App = () => {
+  return (
+    <div>
+     <Head />
+     <Hero />
+     <Footer />
+    </div>
+  )
+}
+
+export default App
+
